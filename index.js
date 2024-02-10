bc7b890df2af2d08b6fbd8d1717026e8d1c1364d
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-10 17:17:55 +0530

    Added route to display users in html document

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express();
 const port = 8000;
 
 // Routes
+app.get("/users", (req, res) => {
+  const html = `
+          <ul>
+              ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+          </ul>
+      `;
+  res.send(html);
+});
+
+// REST API
 app.get("/api/users", (req, res) => {
   return res.json(users);
 });
