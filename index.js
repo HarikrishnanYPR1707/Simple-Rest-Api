09d90ee78586186e28b344c96addba635dcfacf0
Harikrishnan Prasannakumar <hyprgd2002@gmail.com>
2024-02-10 17:07:31 +0530

    imported mock data in index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
 
 const app = express();
 const port = 8000;
+
+// Routes
 
 app.listen(port, () => console.log("Server Started!!"));
